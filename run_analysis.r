998148f559354cb7fb8703a480c4cff472b26df6
shengemma <31185764+shengemma@users.noreply.github.com>
2017-10-07 18:08:42 -0400

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -1,68 +1,48 @@
-
 ##PT1:
 
 ##all the test files
-fattest<-read.table("UCI HAR Dataset/test/X_test.txt")
-str(fattest)
-acttest<-read.table("UCI HAR Dataset/test/Y_test.txt")
-str(acttest)
-sjtest<-read.table("UCI HAR Dataset/test/subject_test.txt")
-str(sjtest)
-test<-data.frame(fattest,acttest,sjtest)
+x_test<-read.table("UCI HAR Dataset/test/X_test.txt")
+y_test<-read.table("UCI HAR Dataset/test/Y_test.txt")
+sj_test<-read.table("UCI HAR Dataset/test/subject_test.txt")
+test<-cbind(sj_test,y_test,x_test)
 
 ##all the train files
-fattrain<-read.table("UCI HAR Dataset/train/X_train.txt")
-str(fattrain)
-acttrain<-read.table("UCI HAR Dataset/train/Y_train.txt")
-str(acttrain)
-sjtrain<-read.table("UCI HAR Dataset/train/subject_train.txt")
-str(sjtrain)
-train<-data.frame(fattrain,acttrain,sjtrain)
+x_train<-read.table("UCI HAR Dataset/train/X_train.txt")
+y_train<-read.table("UCI HAR Dataset/train/Y_train.txt")
+sj_train<-read.table("UCI HAR Dataset/train/subject_train.txt")
+train<-cbind(x_train,y_train,sj_train)
 
-
-##combind test&train
-fat<-rbind(fattest, fattrain)
-act<-rbind(acttest, acttrain)
-sj<-rbind(sjtest,sjtrain)
-#data.all<-rbind(test,train)
-#str(data.all)
-
-##name the var.
-names(sj)<-c("subject")
-names(act)<-c("activity")
-featurename<-read.table("UCI HAR Dataset/features.txt")
-names(fat)<-featurename$V2
-
-##get the data together
-datafatact<-cbind(sj,act)
-data<- cbind(fat,datafatact)
-
+features<-read.table("UCI HAR Dataset/features.txt")
+activity<-read.table("UCI HAR Dataset/features.txt")
 
 ##PT2:
 ##Extracts only the measurements on the mean and standard deviation for each measurement.
 
-ms<-featurename$V2[grep("mean\\(\\)|std\\(\\)", featurename$V2)]
-#str(ms)
-selfatnames<-c(as.character(ms),"subject","activity")
-data<-subset(data, select=selfatnames)
-str(data)
+##combind test&train
+x<-rbind(x_train, x_test)
+y<-rbind(y_train, y_test) 
+sj<-rbind(sj_train,sj_test)
 
-#x<-rbind(xtest,xtrain)
-#data.sub<-data.all[,c(1,2,ms + 2)]
+ms<-grep("mean\\(\\)|std\\(\\)", features[,2])
+
+x<-x[, ms]
+
+names(sj)<-"subject"
+names(x)<-features[ms,2]
 
 
 ##PT3:
 ##Uses descriptive activity names to name the activities in the data set
-ac <- read.table("UCI HAR Dataset/activity_labels.txt")
-str(ac)
-head(ac)
-head(ac$V2)
-head(data$activity)
+y[,1]<-activity[y[,1],2]
+names(y)<-"activity"
 
 ##PT4
 ##Appropriately labels the data set with descriptive variable names
 
+Data<-cbind(x,y,sj)
 
+names(Data)<-gsub("std()", "SD", names(Data))
+names(Data)<-gsub("mean()", "MEAN", names(Data))
 names(Data)<-gsub("^t", "time", names(Data))
 names(Data)<-gsub("^f", "frequency", names(Data))
 names(Data)<-gsub("Acc", "Accelerometer", names(Data))
@@ -70,9 +50,7 @@ names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
 names(Data)<-gsub("Mag", "Magnitude", names(Data))
 names(Data)<-gsub("BodyBody", "Body", names(Data))
 
-names(data)
-
-
+names(Data)
 
 ##PT5
 ##From the data set in step 4, creates a second, 
@@ -80,13 +58,12 @@ names(data)
 ##and each subject.
 
 install.packages("plyr")
-install.packages("data.table")
+#install.packages("data.table")
+
 library(plyr)
-data<-aggregate(.~subject+activity,data,mean)
-data<-data[order(data$subject,data$activity),]
-write.table(data,file = "tidydata.txt",row.name= F)
+avg<- ddply(Data,.(subject,activity), function(x) colMeans(x[, 1:66]))
+write.table(avg,"tide_data_V2.txt", row.name = FALSE)
 
 
 
-#data.tidy <- aggregate(data.sub[,3:81], by = list(activity = data.sub$activity, subject = data.sub$subject),FUN = mean)
-#write.table(x = data.tidy, file = "data_tidy.txt", row.names = FALSE)
+
